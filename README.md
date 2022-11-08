5552f84be0a2ec926ee235350f2ff8148c6db02d
James Hodgkinson <james@terminaloutcomes.com>
2022-11-08 20:56:24 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# homebrew-kanidm
+
+Kanidm Casks for installation - please see https://github.com/kanidm/kanidm for the project.
+
+## Installation: 
+
+Tap the repository:
+
+```
+brew tap kanidm/kanidm
+```
+
+Install the package of your choice:
+
+```
+brew install kanidm
+```
---
+++ kanidm.rb
@@ -0,0 +1,28 @@
+class Kanidm < Formula
+  desc "Kanidm CLI"
+  homepage "https://github.com/kanidm/kanidm"
+  url "https://github.com/kanidm/kanidm/archive/refs/tags/v1.1.0-alpha.10.tar.gz"
+  sha256 "c709c2073739ae713af51f2062aab61bc5299a594db5074625ee87fa340f4330"
+  license "Mozilla Public License 2.0"
+  head "https://github.com/kanidm/kanidm.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--bin", "kanidm", *std_cargo_args
+
+    # Completion scripts and manpage are generated in the crate's build
+    # directory, which includes a fingerprint hash. Try to locate it first
+    out_dir = Dir["target/release/build/kanidm-*/out"].first
+  end
+
+  test do
+    system "#{bin}/kanidm", "--version"
+  end
+end
